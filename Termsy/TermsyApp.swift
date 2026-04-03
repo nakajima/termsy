44d9f34b2162b026c15b9ca54608db0b2fb865b1
Pat Nakajima <patnakajima@gmail.com>
2026-04-02 21:39:07 -0700

    Initial Commit

---
+++ Termsy/TermsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TermsyApp.swift
+//  Termsy
+//
+//  Created by Pat Nakajima on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TermsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TermsyTests/TermsyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TermsyTests.swift
+//  TermsyTests
+//
+//  Created by Pat Nakajima on 4/2/26.
+//
+
+import Testing
+@testable import Termsy
+
+struct TermsyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
